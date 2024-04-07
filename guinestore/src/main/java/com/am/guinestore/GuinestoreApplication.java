fde643f204d529a5dc8ac4bfe2f9a6febb14262d
assimiu <assimium2@gmail.com>
2024-04-07 15:05:19 +0100

    inicial

---
+++ guinestore/src/main/java/com/am/guinestore/GuinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.am.guinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuinestoreApplication.class, args);
+	}
+
+}
---
+++ guinestore/src/test/java/com/am/guinestore/GuinestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.guinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuinestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
